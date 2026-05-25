b3e270895a8c1b6bfff46b2ee0c8b7977d803636
exxteban <csarlezcano@gmail.com>
2026-05-25 10:59:43 -0300

    mejoras panel peluqueria

--- generar_hash.py
+++ generar_hash.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash
 
 # Cambia "MiClave123" por la contraseña que quieras usar
-nuevo_hash = generate_password_hash("root1409")
+nuevo_hash = generate_password_hash("adminpassdemo")
 
 print(nuevo_hash)
 #ultimo pass admin: CLAVEadmin
